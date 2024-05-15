fc90c8468b2349c40872cf15a6a7f4c2a110a267
Mftr96 <matteo.federici96@gmail.com>
2024-05-15 17:08:54 +0200

    ticket commit

--- JS/script.js
+++ JS/script.js
@@ -5,16 +5,32 @@ const{createApp}=Vue;
 createApp({
     data(){
         return{
+            //creo array per salvare mail
+            listaMail:[],
            
         }
     },
     methods:{
+        //importo api boolean mail 
+        f_mail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response)=>  {
+                const result=response.data;
+                console.log(result.response)
+                
+                return result.response;
+              
+            })
+        }
 
     },
     mounted(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(response){
-            const result=response.data;
-            console.log(result)
-        })
+        //creo un ciclo che ripeta la funzione mail 10 volte 
+        for (let i = 1; i <=10; i++) {
+            let element= this.f_mail();
+            
+            this.listaMail.push(element)
+        }
+
+        console.log(this.listaMail)
     }
 }).mount("#app");
