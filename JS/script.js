c1cc1ecb3a7408fe534b3c132c7105197b20bc2f
Mftr96 <matteo.federici96@gmail.com>
2024-05-15 15:24:28 +0200

    riscritto link axiom

--- JS/script.js
+++ JS/script.js
@@ -12,6 +12,9 @@ createApp({
 
     },
     mounted(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(response){
+            const result=response.data;
+            console.log(result)
+        })
     }
 }).mount("#app");
